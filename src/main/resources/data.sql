86f1b8399327014f3aa5c7a22646161a59a1db47
鴇崎 拓也 < @DESKTOP-ROSHTON>
2023-08-26 14:18:56 +0900

    doc: ストアドの説明コメント追記

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,36 +7,37 @@ VALUES ('bbbb', 'Bbb', 'Bbb', 'USER', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpe
 INSERT INTO usr (user_id, first_name, last_name, role_name, password)
 VALUES ('cccc', 'Ccc', 'Ccc', 'ADMIN', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK')/;
 --
-INSERT INTO meeting_room (room_name) VALUES ('新木場')/;
-INSERT INTO meeting_room (room_name) VALUES ('辰巳')/;
-INSERT INTO meeting_room (room_name) VALUES ('豊洲')/;
-INSERT INTO meeting_room (room_name) VALUES ('月島')/;
-INSERT INTO meeting_room (room_name) VALUES ('新富町')/;
-INSERT INTO meeting_room (room_name) VALUES ('銀座一丁目')/;
+INSERT INTO meeting_room (room_name) VALUES ('北千住')/;
+INSERT INTO meeting_room (room_name) VALUES ('上野')/;
+INSERT INTO meeting_room (room_name) VALUES ('東日本橋')/;
+INSERT INTO meeting_room (room_name) VALUES ('新宿')/;
+INSERT INTO meeting_room (room_name) VALUES ('九段下')/;
+INSERT INTO meeting_room (room_name) VALUES ('東京')/;
 INSERT INTO meeting_room (room_name) VALUES ('有楽町')/;
+
 -- Stored Procedure
 DROP FUNCTION IF EXISTS REGISTER_RESERVABLE_ROOMS()/;
-CREATE OR REPLACE FUNCTION REGISTER_RESERVABLE_ROOMS()
-  RETURNS
-    INT AS $$
-DECLARE
+CREATE OR REPLACE FUNCTION REGISTER_RESERVABLE_ROOMS()-- CREATE OR REPLACE FUNCTIONは、新しい関数の作成、または既存定義の置換のどちらかを行う
+  RETURNS--戻り値
+    INT AS $$--「as $$」はストアドの始点 
+DECLARE--変数宣言
   total INT;
   i INT4;
   id INT4;
-BEGIN
+BEGIN--ストアドの処理開始
   total := 0;
   FOR id IN SELECT room_id
             FROM meeting_room LOOP
     i := 0;
-    FOR i IN 0..77 LOOP
+    FOR i IN 0..6 LOOP--実行日の3日前～3日先までinsert
       INSERT INTO reservable_room (reserved_date, room_id)
-      VALUES (CURRENT_DATE + i - 7, id);
+      VALUES (CURRENT_DATE + i - 3, id);--実行日の3日前からinsert
     END LOOP;
     total := total + i;
   END LOOP;
-  RETURN total;
-END;
-$$ LANGUAGE plpgsql
+  RETURN total;--ストアドの戻り値
+END;--ストアドの処理終了
+$$ LANGUAGE plpgsql--ストアドの終点と、このストアドがPL/pgSQLで記述されていることを指定
 /;
 SELECT REGISTER_RESERVABLE_ROOMS() /;
 COMMIT /;
