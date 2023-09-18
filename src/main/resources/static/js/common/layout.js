7dd91e5558270f3400dca1054ca9a46ce9a0451a
鴇崎 拓也 < @DESKTOP-ROSHTON>
2023-09-18 14:18:30 +0900

    feat: js処理の追加(取消ボタン押下時に確認ダイアログ表示)

--- src/main/resources/static/js/common/layout.js
+++ src/main/resources/static/js/common/layout.js
@@ -1,1 +1,17 @@
 console.log("layout.js 読み込み");
+
+
+function checkBeforeAction(actionNm) {
+	// 確認ダイアログを表示
+	if (window.confirm(String(actionNm) + 'を実施します。よろしいですか？')) {
+		// 「OK」時は実行
+		return true;
+
+	}
+	else {
+		// 「キャンセル」時の処理
+		window.alert('キャンセルされました'); // 警告ダイアログを表示
+		// 送信を中止
+		return false;
+	}
+}
